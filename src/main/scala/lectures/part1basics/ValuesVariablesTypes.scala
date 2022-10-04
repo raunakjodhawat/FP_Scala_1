bea414c2340f5802d1044c6a9bee930fb4feca53
Raunak Jodhawat <raunakjodhawat@gmail.com>
2022-10-04 13:04:55 +0530

    lecture 1

---
+++ src/main/scala/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,27 @@
+package com.raunakjodhawat
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+  val x: Int = 42
+  println(x)
+
+  // VALS are immutable
+
+  // COMPILER can infer types
+
+  val aString: String = "Hello, this is a string!"
+
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  val anInt: Int = x
+  val aShort: Short = 123
+  val aLong: Long = 1231231231231231L
+  val aFloat: Float = 2.0
+  val aDouble: Double = 3.5
+
+  // variables
+
+  var aVariable: Int = 4
+  aVariable = 5 // side effects
+  println(aVariable)
+}
---
+++ src/main/scala/playground/playground.scala
@@ -0,0 +1,8 @@
+package com.raunakjodhawat
+package playground
+
+object playground {
+  def main(args: Array[String]): Unit = {
+    println("Hello, World!")
+  }
+}
