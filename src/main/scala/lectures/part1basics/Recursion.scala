34ecd79f6f3833ba071614b0a648038d7ff7bcee
Raunak Jodhawat <raunakjodhawat@gmail.com>
2022-10-07 17:45:30 +0530

    lecture 7

--- src/main/scala/lectures/part1basics/Recursion.scala
+++ src/main/scala/lectures/part1basics/Recursion.scala
@@ -26,4 +26,52 @@ object Recursion extends App {
   factorial(10)
   println(anotherFactorial(200))
 
+  // Exercises
+
+  def concatenate(str: String, n: Int): String = {
+    @tailrec
+    def loop(s: String, m: Int): String = {
+      if (m < n - 1) {
+        loop(s + str, m + 1)
+      } else {
+        s
+      }
+    }
+    loop(str, 0)
+  }
+  println(concatenate("hello", 3))
+
+  def isPrime(n: Int): Boolean = {
+    @tailrec
+    def loop(m: Int): Boolean = {
+      if (m < n / 2) {
+        if (n % m == 0) {
+          false
+        } else {
+          loop(m + 1)
+        }
+      } else true
+    }
+    loop(2)
+  }
+
+  def fibonacci(n: Int): Int = {
+    if (n <= 2) {
+      1
+    } else {
+      @tailrec
+      def loop(f: Int, s: Int, counter: Int): Int = {
+        if (counter == n) {
+          f + s
+        } else {
+          loop(s, f + s, counter + 1)
+        }
+      }
+      loop(1, 1, 3)
+    }
+  }
+
+  println(isPrime(23))
+
+  println(fibonacci(8))
 }
