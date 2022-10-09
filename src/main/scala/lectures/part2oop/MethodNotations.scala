26810e4a1beb60d8bedbc5381a0f33f23613ece5
Raunak Jodhawat <raunakjodhawat@gmail.com>
2022-10-09 17:26:10 +0530

    lecture 13

--- src/main/scala/lectures/part2oop/MethodNotations.scala
+++ src/main/scala/lectures/part2oop/MethodNotations.scala
@@ -2,13 +2,18 @@ package com.raunakjodhawat
 package lectures.part2oop
 
 object MethodNotations extends App {
-  class Person(val name: String, favMovie: String) {
+  class Person(val name: String, favMovie: String, val age: Int = 0) {
     def likes(movie: String): Boolean = movie == favMovie
     def +(p: Person): String = s"$name is hanging out with ${p.name}"
+    def +(s: String): Person = new Person(s"$name ($s)", favMovie)
     def unary_! : String = s"$name, what the heck!"
+    def unary_+ : Person = new Person(name, favMovie, age + 1)
     def isAlive: Boolean = true
-
+    def learns(s: String): String = s"$name learns $s"
+    def learnScala: String = this learns "Scala"
     def apply(): String = s"Hi, my name is $name and I like $favMovie"
+    def apply(i: Int): String =
+      s"$name watched their favorite movie: $favMovie, $i times"
   }
 
   val mary = new Person("Mary", "inception")
@@ -44,4 +49,8 @@ object MethodNotations extends App {
   // apply
   println(mary.apply())
   println(mary())
+
+  // Exercises
+  println(mary.learnScala)
+  println((+mary).age)
 }
