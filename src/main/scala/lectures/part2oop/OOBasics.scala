0af5853b8cc6e4a649927baf3186a907cab8faa9
Raunak Jodhawat <raunakjodhawat@gmail.com>
2022-10-09 14:35:48 +0530

    lecture 10

--- src/main/scala/lectures/part2oop/OOBasics.scala
+++ src/main/scala/lectures/part2oop/OOBasics.scala
@@ -3,12 +3,32 @@ package lectures.part2oop
 
 object OOBasics extends App {
   val person = new Person("Raunak", 28)
-  println(person.age)
+  println(person.x)
+
+  println(person.greet("Jodhawat"))
+  println(person.greet())
 }
 // constructor
-class Person(name: String, val age: Int) {
+class Person(name: String, val age: Int = 0) {
   // body
+  val x = 2
 
+  println(2 + 2)
+
+  def greet(name: String): Unit = {
+    println(s"${this.name} says hi: $name")
+  }
+
+  // overloading (same name, but different signature)
+  def greet(): Unit = {
+    println(s"Hi, i am $name")
+  }
+
+  // multiple constructors
+
+  def this(name: String) = this(name, 0)
+
+  def this() = this("John Doe")
 }
 
 // class parameters are not fields
