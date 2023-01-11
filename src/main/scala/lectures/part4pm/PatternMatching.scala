9859753a9d8be0515253829263dd58c8da7493d1
Raunak Jodhawat <raunakjodhawat@gmail.com>
2023-01-12 00:56:50 +0530

    lecture 36

---
+++ src/main/scala/lectures/part4pm/PatternMatching.scala
@@ -0,0 +1,77 @@
+package com.raunakjodhawat
+package lectures.part4pm
+
+import scala.util.Random
+
+object PatternMatching extends App {
+  // switch on steroids
+
+  val random = new Random()
+  val x = random.nextInt(10)
+
+  val description = x match {
+    case 1 => "the one"
+    case 2 => "double or nothing"
+    case 3 => "third time is the charm"
+    case _ => "something else" // _ is a wildcard
+  }
+
+  println(x)
+  println(description)
+
+  // 1. decompose values
+  case class Person(name: String, age: Int)
+  val bob = Person("Bob", 20)
+
+  val greeting = bob match {
+    case Person(n, a) if a < 21 =>
+      s"Hi, my name is $n and I can't drink in the US"
+    case Person(n, a) => s"Hi, my name is $n and I am $a years old"
+    case _            => "I don't know who I am"
+  }
+  println(greeting)
+
+  /**   1. Cases are matched in order 2. If no cases match => scala.MatchError
+    *      PM works really well with case classes
+    */
+
+  // PM on sealed hierarchies
+  sealed class Animal
+  case class Dog(breed: String) extends Animal
+  case class Parrot(greeting: String) extends Animal
+
+  val animal: Animal = Dog("Terro Nova")
+
+  animal match {
+    case Dog(someBreed) => println(s"Matched a dog of the $someBreed")
+  }
+
+  val isEven = x % 2 == 0
+  val isAlsoEven = x match {
+    case n if n % 2 == 0 => true
+    case _               => false
+  }
+
+  // Exercises
+  trait Expr
+  case class Number(n: Int) extends Expr
+  case class Sum(e1: Expr, e2: Expr) extends Expr
+  case class Product(e1: Expr, e2: Expr) extends Expr
+
+  def show(ex: Expr): String = ex match {
+    case Number(n)   => n.toString
+    case Sum(e1, e2) => s"${show(e1)} + ${show(e2)}"
+    case Product(e1, e2) =>
+      def maybeShowParantheses(exp: Expr) = exp match
+        case Product(_, _) => show(exp)
+        case Number(_)     => show(exp)
+        case _             => s"(${show(exp)})"
+      s"${maybeShowParantheses(e1)} * ${maybeShowParantheses(e2)}"
+
+  }
+
+  println(show(Sum(Number(2), Number(3))))
+  println(show(Sum(Number(2), Sum(Number(3), Number(4)))))
+  println(show(Product(Sum(Number(2), Number(1)), Number(3))))
+  println(show(Sum(Product(Number(2), Number(1)), Number(3))))
+}
